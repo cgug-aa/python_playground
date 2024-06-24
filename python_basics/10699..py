2ed5da46a7440f8262622376f5ea3a502bc9fea2
cgug-aa <cksrn0003@naver.com>
2024-06-24 11:13:05 +0900

    240624

---
+++ python_basics/10699..py
@@ -0,0 +1,6 @@
+#오늘 날짜
+
+year='2024'
+month='06'
+day='24'
+print("%s-%s-%s" %(year, month, day))
---
+++ python_basics/8958.py
@@ -0,0 +1,14 @@
+#OX 퀴즈
+
+T=int(input())
+for i in  range(T):
+    sum=0
+    score=0
+    string=input()
+    for j in range(len(string)):
+        if string[j]=='O':
+            score+=1
+            sum+=score
+        else:
+            score=0
+    print(sum)
