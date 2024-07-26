b5fb507b774ecff610b3fd890b21f59160d357c3
cgug_aa <cksrn0003@naver.com>
2024-07-26 15:43:47 +0900

    240726

--- 2047_basics_of_university/1205_.py
+++ 2047_basics_of_university/1205_.py
@@ -6,29 +6,37 @@ class solution():
         self.score=score
         self.p=p
     
-    def ranking_setting(self, string):
-        self.ranking=list(map(int, string.split()))
-        self.ranking.append(self.score)
-        self.ranking.sort(reverse=True)
+    def ranking_setting(self):
+        if self.n>0:
+            self.ranking=list(map(int, input().split()))
+            self.ranking.append(self.score)
+            self.ranking.sort(reverse=True)
+        else:
+            self.ranking=[]
+            self.ranking.append(self.score)
 
-    def slicing_ranking(self):
-        self.ranking=self.ranking[:self.p]
+    def slicing_ranking(self, list):
+        if self.n>=self.p:
+            if list[self.p-1]==list[self.p]:
+                idx=self.ranking.index(list[self.p])
+                self.ranking=list[:idx]
+            else:
+                self.ranking=list[:self.p]
+        else:
+            pass
 
     def chulsoo_position(self, list):
-        '''
-        c_idx=list.index(self.score)
-        if type(c_idx)==int and c_idx!=len(list)-1:
+        try:
+            c_idx=list.index(self.score)
             print(c_idx+1)
-        else:
+        except ValueError:
             print(-1)
-        ''' 
+
 
     def answer(self):
-        self.ranking_setting(input())
-        self.slicing_ranking()
+        self.ranking_setting()               #EOFError: "self.ranking_setting(input())" 현재 차트에 아무것도 없을 때, 입력이 없는데 입력을 받으려고 하고 있음.
+        self.slicing_ranking(self.ranking)
         self.chulsoo_position(self.ranking)
-
-
 
 
 N, score, P= map(int, input().split())
@@ -37,4 +45,8 @@ sol.answer()
 
 # 알게 된 점
 # 1) 리스트 원소 자료형 변환: list(map(자료형, 리스트)) 
-# 2) 리스트 내장 함수 index는 찾는 원소가 리스트에 없으면 ValueError가 발생한다. 
+# 2) 리스트 내장 함수 index는 찾는 원소가 리스트에 없으면 ValueError가 발생한다.
+# 3) EOFError 란
+#     EOFError는 "End Of File Error"의 약자로, 파일을 읽을 때 파일의 끝을 만나면 발생하는 에러.
+#     말 그대로 파일의 끝에 도달했을 때 더 이상 읽을 내용이 없어서 발생하는 에러.
+#     혹은 파일을 읽는 도중 사용자가 예상치 못한 입력을 했을 때 발생할 수도 있다.  
